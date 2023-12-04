125114994037af69f16a192fd224bbe1a31ee877
pariyahr <pariyahajipour@gmail.com>
2023-12-05 01:16:29 +0330

    messages added

--- holysheet/views.py
+++ holysheet/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from .models import Customer
+from django.contrib import messages
+from django.utils.translation import get_language
 
 
 # Create your views here.
@@ -19,7 +21,10 @@ def login(request):
         try:
             customer = Customer.objects.get(username=username)
             if customer.password == password:
+                messages.info(request, 'Successfully logged in')
                 return redirect('home_page')
+            else:
+                messages.info(request, 'Wrong username of password')
         except Customer.DoesNotExist:
             # Handle the case where the username doesn't exist
             # You can add appropriate error handling or redirect the user to an error page
@@ -42,12 +47,23 @@ def register(request):
         confirm_password = request.POST.get('password')
         email = request.POST.get('email')
         number = request.POST.get('phone_number')
-        user = Customer.objects.create(username=username, password=password, email=email, first_name=firstname,
-                                       last_name=lastname, gender=gender, contact_number=number)
 
-        return redirect('login')
+        try:
+            customer = Customer.objects.get(username=username)
+            messages.info(request, 'Username is taken')
+        except:
+            if len(password) < 8:
+                messages.info(request, 'Password is weak')
+            elif number[0] != '0' or number[1] != '9' or len(number) != 11:
+                messages.info(request, 'phone number format is wrong')
+            else:
+                user = Customer.objects.create(username=username, password=password, email=email, first_name=firstname,
+                                               last_name=lastname, gender=gender, contact_number=number)
+                messages.info(request, 'User registered successfully')
+                return redirect('login')
 
     return render(request, 'register.html')
+
 
 def home(request):
     template = loader.get_template('home_page.html')
